385bbb580c654151716413d6ae83e495250602de
nfeldt2 <n.feldt@utexas.edu>
2025-11-03 17:39:02 -0600

    ctm_run_4_test code

--- train_ctm.sh
+++ train_ctm.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
-save_dir="model_runs/ctm_run_3"
+save_dir="model_runs/ctm_run_4_test"
 mkdir -p $save_dir
 
-nohup torchrun --nproc_per_node=4 train_ctm.py \
-    --data_dir "/mnt/hot/public/Akul/exhale_pred_data" \
+torchrun --nproc_per_node=4 train_ctm.py \
+    --data_dir "/mnt/hot/public/Akul/exhale_pred_data_10pct" \
     --save_dir $save_dir \
-    --epochs 200 \
-    --batch_size 5 \
+    --epochs 50 \
+    --batch_size 1 \
     --n_workers 8 \
-    --val_interval 5 \
+    --val_interval 2 \
     --lr 1e-4 \
-    --dataset_fraction 0.2 \
+    --dataset_fraction 1 \
     --alpha 2.0 \
     --lambda_cycle 1.0 \
-    --lambda_expansion 10.0 > $save_dir/train.log 2>&1 &
+    --lambda_expansion 10.0
